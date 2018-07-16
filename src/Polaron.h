972ec5c4a887bae83883d22aeecbc0f5736f4b59
Michael C. Heiber <mike.heiber@gmail.com>
2018-07-16 11:53:57 -0400

    Bkl algorithm (#34)
    Closes #30 and #31
    
    KMC_Lattice submodule:
    -updated submodule to latest development branch version that has the BKL algorithm implemented
    
    Exciton class:
    -removed calculateExecutionTime functions for each event and replace
    them with calculateRateConstant functions
    
    Polaron class:
    -removed calculateExecutionTime functions for each event and replace
    them with calculateRateConstant functions
    
    OSC_Sim class:
    -updated calculateExcitonEvents and calculatePolaronEvents to use new
    BKL algorithm
    
    testing:
    -updated tests to suppress cout command line output

--- src/Polaron.h
+++ src/Polaron.h
@@ -13,77 +13,72 @@
 
 namespace Excimontec {
 
-class Polaron : public Object{
-    public:
-        static const std::string object_type;
-        Polaron(const double time,const int tag_num,const Coords& start_coords,const bool polaron_charge) : Object(time,tag_num,start_coords){charge = polaron_charge;}
-        bool getCharge() const{return charge;}
-		std::string getObjectType() const{return object_type;}
-    private:
-        bool charge; // false represents negative charge, true represents positive charge
+class Polaron : public Object {
+public:
+	static const std::string object_type;
+	Polaron(const double time, const int tag_num, const Coords& start_coords, const bool polaron_charge) : Object(time, tag_num, start_coords) { charge = polaron_charge; }
+	bool getCharge() const { return charge; }
+	std::string getObjectType() const { return object_type; }
+private:
+	bool charge; // false represents negative charge, true represents positive charge
 };
 
-class Polaron_Hop : public Event{
-    public:
-        static const std::string event_type;
-		Polaron_Hop() : Event() {}
-		Polaron_Hop(Simulation* simulation_ptr) : Event(simulation_ptr) {}
-        void calculateExecutionTime(const double prefactor,const double localization,const double distance,const double E_delta){
-            // Calculates hopping using the Miller-Abrahams model
-            double rate = prefactor*exp(-2.0*localization*distance);
-            if(E_delta>0){
-                rate *= exp(-E_delta/(Utils::K_b*sim_ptr->getTemp()));
-            }
-            Event::calculateExecutionTime(rate);
-        }
-        void calculateExecutionTime(const double prefactor,const double localization,const double distance,const double E_delta,const double reorganization){
-            // Calculates hopping using the Marcus model
-            double rate = (prefactor/sqrt(4.0*Utils::Pi*reorganization*Utils::K_b*sim_ptr->getTemp()))*exp(-2.0*localization*distance)*exp(-Utils::intpow(reorganization+E_delta,2)/(4.0*reorganization*Utils::K_b*sim_ptr->getTemp()));
-            Event::calculateExecutionTime(rate);
-        }
-		std::string getEventType() const{return event_type;}
-    private:
-
-};
-
-class Polaron_Recombination : public Event{
-    public:
-        static const std::string event_type;
-		Polaron_Recombination() : Event() {}
-		Polaron_Recombination(Simulation* simulation_ptr) : Event(simulation_ptr) {}
-        void calculateExecutionTime(const double prefactor,const double localization,const double distance,const double E_delta){
-            // Calculates recombination using the Miller-Abrahams model
-            double rate = prefactor*exp(-2.0*localization*distance);
-            if(E_delta>0){
-                rate *= exp(-E_delta/ (Utils::K_b*sim_ptr->getTemp()));
-            }
-            Event::calculateExecutionTime(rate);
-        }
-        void calculateExecutionTime(const double prefactor,const double localization,const double distance,const double E_delta,const double reorganization){
-            // Calculates recombination using the Marcus model
-            double rate = (prefactor/sqrt(4.0*Utils::Pi*reorganization*Utils::K_b*sim_ptr->getTemp()))*exp(-2.0*localization*distance)*exp(-Utils::intpow(reorganization+E_delta,2)/(4.0*reorganization*Utils::K_b*sim_ptr->getTemp()));
-            Event::calculateExecutionTime(rate);
-        }
-		std::string getEventType() const{return event_type;}
-    private:
+class Polaron_Hop : public Event {
+public:
+	static const std::string event_type;
+	Polaron_Hop() : Event() {}
+	Polaron_Hop(Simulation* simulation_ptr) : Event(simulation_ptr) {}
+	void calculateRateConstant(const double prefactor, const double localization, const double distance, const double E_delta) {
+		// Calculates hopping using the Miller-Abrahams model
+		rate_constant = prefactor * exp(-2.0*localization*distance);
+		if (E_delta > 0) {
+			rate_constant *= exp(-E_delta / (Utils::K_b*sim_ptr->getTemp()));
+		}
+	}
+	void calculateRateConstant(const double prefactor, const double localization, const double distance, const double E_delta, const double reorganization) {
+		// Calculates hopping using the Marcus model
+		rate_constant = (prefactor / sqrt(4.0*Utils::Pi*reorganization*Utils::K_b*sim_ptr->getTemp()))*exp(-2.0*localization*distance)*exp(-Utils::intpow(reorganization + E_delta, 2) / (4.0*reorganization*Utils::K_b*sim_ptr->getTemp()));
+	}
+	std::string getEventType() const { return event_type; }
+private:
 
 };
 
-class Polaron_Extraction : public Event{
-    public:
-        static const std::string event_type;
-		Polaron_Extraction() : Event() {}
-		Polaron_Extraction(Simulation* simulation_ptr) : Event(simulation_ptr) {}
-        void calculateExecutionTime(const double prefactor,const double localization,const double distance,const double E_delta){
-            // Calculates extraction using the Miller-Abrahams model
-            double rate = prefactor*exp(-2.0*localization*distance);
-            if(E_delta>0){
-                rate *= exp(-E_delta/(Utils::K_b*sim_ptr->getTemp()));
-            }
-            Event::calculateExecutionTime(rate);
-        }
-		std::string getEventType() const{return event_type;}
-    private:
+class Polaron_Recombination : public Event {
+public:
+	static const std::string event_type;
+	Polaron_Recombination() : Event() {}
+	Polaron_Recombination(Simulation* simulation_ptr) : Event(simulation_ptr) {}
+	void calculateRateConstant(const double prefactor, const double localization, const double distance, const double E_delta) {
+		// Calculates recombination using the Miller-Abrahams model
+		rate_constant = prefactor * exp(-2.0*localization*distance);
+		if (E_delta > 0) {
+			rate_constant *= exp(-E_delta / (Utils::K_b*sim_ptr->getTemp()));
+		}
+	}
+	void calculateRateConstant(const double prefactor, const double localization, const double distance, const double E_delta, const double reorganization) {
+		// Calculates recombination using the Marcus model
+		rate_constant = (prefactor / sqrt(4.0*Utils::Pi*reorganization*Utils::K_b*sim_ptr->getTemp()))*exp(-2.0*localization*distance)*exp(-Utils::intpow(reorganization + E_delta, 2) / (4.0*reorganization*Utils::K_b*sim_ptr->getTemp()));
+	}
+	std::string getEventType() const { return event_type; }
+private:
+
+};
+
+class Polaron_Extraction : public Event {
+public:
+	static const std::string event_type;
+	Polaron_Extraction() : Event() {}
+	Polaron_Extraction(Simulation* simulation_ptr) : Event(simulation_ptr) {}
+	void calculateRateConstant(const double prefactor, const double localization, const double distance, const double E_delta) {
+		// Calculates extraction using the Miller-Abrahams model
+		rate_constant = prefactor * exp(-2.0*localization*distance);
+		if (E_delta > 0) {
+			rate_constant *= exp(-E_delta / (Utils::K_b*sim_ptr->getTemp()));
+		}
+	}
+	std::string getEventType() const { return event_type; }
+private:
 };
 
 }
